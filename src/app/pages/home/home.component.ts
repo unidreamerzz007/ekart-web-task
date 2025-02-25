f2cf61d599308ac23f34ce18d0ab8712f0203df7
unidreamerzz007 <unidreamerzz@gmail.com>
2025-02-25 15:45:38 +0530

    search function & changed api

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,FooterComponent,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -2,30 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { NgClass, NgFor, NgIf } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   standalone: true,
   templateUrl: './products.component.html',
-  imports: [NavbarComponent, FooterComponent, NgFor,NgIf], 
+  imports: [NavbarComponent, FooterComponent, NgFor, NgIf, RouterModule],
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent implements OnInit {
 
   products: any[] = [];
+  filteredProducts: any[] = [];
   isLoading: boolean = true;
 
+  constructor(private router: Router) {}
+
   viewProduct(productId: number) {
-    window.location.href = `/products/${productId}`;
+    this.router.navigate(['/products', productId]);
+  }
+
+  handleSearch(query: string) {
+    this.filteredProducts = this.products.filter((product) =>
+      product.title.toLowerCase().includes(query.toLowerCase())
+    );
   }
 
   ngOnInit() {
     this.isLoading = true;
 
-    fetch('https://dummyjson.com/products?500')
+    fetch('https://fakestoreapi.com/products')
       .then((res) => res.json())
       .then((data) => {
-        this.products = data.products;
+        this.products = data;
+        this.filteredProducts = data;
         this.isLoading = false;
       })
       .catch((error) => {
