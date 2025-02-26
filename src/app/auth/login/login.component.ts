27b03eec3756ff44a11d50592afa7f9d856e074e
unidreamerzz007 <unidreamerzz@gmail.com>
2025-02-26 11:14:54 +0530

    added some updation in similar product function and button

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, NgModel, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent,RouterModule],
+  imports: [NavbarComponent, FooterComponent, RouterModule,NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/
+          )
+        ]
+      ]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      console.log('Form Submitted!', this.loginForm.value);
+    }
+  }
 }
